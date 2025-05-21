662826c21c7df0a1870e7f613b4fb60c0af12057
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-05-21 13:26:01 +0300

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='py-10 header-bg h-[100vh] w-[100vw] flex items-center justify-center'>
+    </div>
+  )
+}
+
+export default App
